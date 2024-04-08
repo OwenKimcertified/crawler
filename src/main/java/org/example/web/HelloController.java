8917f2d00a168d26e1d0e07d5f560c2f0b68a3e5
owen <ehdrb3309@naver.com>
2024-04-08 09:52:54 +0900

    24-04-08

--- src/main/java/org/example/web/HelloController.java
+++ src/main/java/org/example/web/HelloController.java
@@ -1,9 +1,14 @@
 package org.example.web;
 
 
+import org.example.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping; // HTTP Get 요청에 반응하는 API
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController; // 컨트롤러를 json 으로 리턴
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 @RestController
+@EnableWebMvc
 public class HelloController {
     //API
     @GetMapping("/hello") //
@@ -11,4 +16,8 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, //
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount); }
 }
