de480a92eafedc1cc4b5e823a38763a74b88fb56
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2024-04-19 02:30:01 +0900

    Update coupang.py

--- crawling_file/coupang.py
+++ crawling_file/coupang.py
@@ -8,44 +8,4 @@ term = time.sleep(randint)
 
 server_url = "http://localhost:8080/api/product/put/coupang/notebook"
 
-headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-           "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-           "Referer": "https://www.google.com",
-           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
-
-headers2 = {"Content-Type": "application/json"}
-
-product_list = ["냉장고"] # 비동기 처리 예정
-
-
-with sync_playwright() as p:
-    browser = p.chromium.launch()
-    context = browser.new_context(extra_http_headers = headers)
-    page = context.new_page()
-    for i in product_list:
-        for j in range(1, 2):
-            page.goto(f"https://www.coupang.com/np/search?q={i}&channel=recent&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page={j}&rocketAll=false&searchIndexingToken=1=9&backgroundColor=")
-            dct = dict()
-            item_list = page.locator("li.search-product").all()
-            for p in item_list:
-                dct["productName"] = p.locator(".name").inner_text()
-                dct["origin_price"] = int(re(",", "", p.locator(".price-value").inner_text()))
-                
-                if is_exist_element(p, ".base-price") == True:
-                    dct["discount_price"] = p.locator(".base-price").inner_text()
-                else:
-                    dct["discount_price"] = "None"
-                
-                if is_exist_element(p, ".rocket.badge") == True:
-                    dct["rocket"] = "True"
-                else:
-                    dct["rocket"] = "False"
-                
-                dct['rating'] = float(p.locator(".rating").inner_text())
-
-                print(json.dumps(dct, ensure_ascii = False))
-                # requests.put(server_url, json = dct, headers = headers2)
-                print(f"put data {dct['productName']}\n{dct['price']}\n\{dct['rating']}\n{dct['rocket']} in server")
-                term
-
-    browser.close()
+# secret
