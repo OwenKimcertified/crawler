f6310ae451240a749ac6f7270525f10da6b043dc
Owen <99598620+OwenKimcertified@users.noreply.github.com>
2024-04-19 02:31:12 +0900

    Update coupang.py

--- crawling_file/coupang.py
+++ crawling_file/coupang.py
@@ -1,11 +1,8 @@
-from playwright.sync_api import sync_playwright
-import random, time, re, json, requests 
+# secret
 from crawling_utils import is_exist_element
 # import asynico
 
-randint = random.uniform(1.4, 5.8)
-term = time.sleep(randint)
+# secret
 
-server_url = "http://localhost:8080/api/product/put/coupang/notebook"
-
+# secret
 # secret
